c303c0fa9977ba0b361b95b1e2fc686b1199163c
madhavanpallan <madhpallan@gmail.com>
2019-04-05 07:20:00 -0400

    added texstudio

--- temp.sh
+++ temp.sh
@@ -2,4 +2,4 @@ sudo apt-get install guake
 wget https://az764295.vo.msecnd.net/stable/a3db5be9b5c6ba46bb7555ec5d60178ecc2eaae4/code_1.32.3-1552606978_amd64.deb
 sudo dpkg -i code_1.32.3-1552606978_amd64.deb
 sudo snap install postman
-sudo apt install gnome-clocks
+sudo apt install gnome-clocks texstudio
