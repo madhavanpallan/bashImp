7d6b003dc92497ceb50601a4e75d9ffa0a0c7eb9
madhavanpallan <madhpallan@gmail.com>
2019-04-16 15:49:56 -0400

    commented removing sublime editor scripts

--- install.sh
+++ install.sh
@@ -13,3 +13,6 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+#Remove sublime text and autoremove
+#sudo apt-get remove sublime-text && sudo apt-get autoremove
