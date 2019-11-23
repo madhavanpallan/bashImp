da46bb2d0971f6ed084227689454ff82c2240465
Madhavan Pallan <madhpallan@gmail.com>
2019-11-23 08:41:07 -0500

    removed test # for change

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #Better terminal window for shell commands 
 sudo apt-get install guake
+
 #added visual studio.
-#wget https://az764295.vo.msecnd.net/stable/a3db5be9b5c6ba46bb7555ec5d60178ecc2eaae4/code_1.32.3-1552606978_amd64.deb
-#sudo dpkg -i code_1.32.3-1552606978_amd64.deb
+wget https://az764295.vo.msecnd.net/stable/a3db5be9b5c6ba46bb7555ec5d60178ecc2eaae4/code_1.32.3-1552606978_amd64.deb
+sudo dpkg -i code_1.32.3-1552606978_amd64.deb
+
 #For all the REST API usage.
 sudo snap install postman
+
 #Tool for latex formatting and editing
 sudo apt install gnome-clocks texstudio
 
